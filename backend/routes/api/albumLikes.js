ca916d92c3f9d400631880ba9bdd80206bcc4735
msineath <mikesineath@gmail.com>
2021-10-02 01:56:27 -0400

    added patch route to /api/albumLikes.js

--- backend/routes/api/albumLikes.js
+++ backend/routes/api/albumLikes.js
@@ -8,4 +8,74 @@ router.get('/', asyncHandler(async (req, res) => {
     return res.json(likes);
 }));
 
+router.patch('/:albumId', asyncHandler(async (req, res) => {
+    const { userId, targetKey } = req.body;
+    const id = Number(req.params.albumId);
+    
+    const like = await AlbumLike.findOne(
+        {
+            where: 
+            {
+                albumId: id,
+                userId
+            }
+        }
+    );
+    
+    if(like.length < 1) {
+        
+        const newLikeStats = {
+            userId,
+            albumId : id
+        };
+
+        if(targetKey === 'liked') {
+            newLikeStats['like'] = true;
+            newLikeStats['unlike'] = false;
+        } else {
+            newLikeStats['liked'] = false;
+            newLikeStats['disliked'] = true;
+        };
+
+        const newLike = await AlbumLike.create(newLikeStats)
+        return res.json(newLike);
+
+     } else {
+        if(targetKey === 'disliked') {
+            const updatedInfo = await AlbumLike.update(
+                {
+                    liked: false,
+                    disliked: true
+                },
+                {
+                    where:
+                        {
+                            userId,
+                            albumId: id
+                        }
+                }
+            );
+
+            return res.json(updatedInfo);
+
+        } else {
+
+            const updatedInfo = await AlbumLike.update(
+                {
+                    liked: true,
+                    disliked: false
+                },
+                {
+                    where:
+                        {
+                            userId,
+                            albumId: id                         
+                        }
+                }
+            );
+            return res.json(updatedInfo);
+        }
+     }
+}));
+
 module.exports = router;
