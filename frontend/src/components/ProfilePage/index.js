ee5c89c6a616a101a16616d339fe161f49697547
msineath <mikesineath@gmail.com>
2021-10-03 21:16:39 -0400

    added liked albums to profile page

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -19,6 +19,14 @@ export default function ProfilePage() {
     const songsArray = Object.values(songs);
     const userUploaded = songsArray.filter(song => song.uploaderId === Number(userId));
 
+    const albums = useSelector(state => state.albums);
+    const albumsArray = Object.values(albums);
+
+    const albumLikes = useSelector(state => state.albumLikes);
+    const albumLikesArray = Object.values(albumLikes);
+    const selectedAlbumLikes = albumLikesArray.filter(like => like.userId === Number(userId));
+    const selectedAlbumLikeNames = selectedAlbumLikes.map (like => albumsArray.filter(album => like.albumId === album.id)).flat();
+
     useEffect(() => {
         dispatch(getSongs())
         dispatch(getAlbums())
@@ -28,13 +36,18 @@ export default function ProfilePage() {
 
     return(
         <ul>
-            <li>Songs Uploaded by {user.username}</li>
+            <li>Songs Uploaded By {user.username}</li>
             {userUploaded.map(song => 
                 <ul>
                     <li><Link to={`/songs/${song.id}`}>{song.title}</Link></li>
                 </ul>
             )}
-
+            <li>Albums {user.username} Likes</li>
+            {selectedAlbumLikeNames.map(album => 
+                <ul>
+                    <li><Link to={`/albums/${album.id}`}>{album.name}</Link></li>
+                </ul>   
+            )}
         </ul>
     )
 };
