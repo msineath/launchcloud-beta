6afc6519d8960534c0dc780869678aa05cae7150
msineath <mikesineath@gmail.com>
2021-10-06 20:02:51 -0400

    added post route to SongComments.js

--- backend/routes/api/songComments.js
+++ backend/routes/api/songComments.js
@@ -8,4 +8,11 @@ router.get('/', asyncHandler(async (req, res) => {
     return res.json(comments);
 }));
 
+router.post('/add/:songId', asyncHandler(async (req, res) => {
+    const {commentText, userId} = req.body;
+    const songId = req.params.songId;
+    const newComment = await SongComment.create({songId, comment: commentText, userId});
+    return res.json(newComment);
+}));
+
 module.exports = router;
