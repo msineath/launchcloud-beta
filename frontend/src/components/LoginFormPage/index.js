0ce2573098f4f61d704700bde9f0c757e3da6c33
msineath <mikesineath@gmail.com>
2021-10-07 23:20:55 -0400

    basic LoginFormPage component structure

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {Redirect} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {loginThunk, demoUserLogin} from '../../store/session';
 import './LoginForm.css';
 import backgroundImage from './background-image.jpg'
@@ -15,10 +15,6 @@ export default function LoginFormPage() {
     
     const dispatch = useDispatch();
 
-    if(sessionUser) return (
-        <Redirect to='/' />
-    )
-
     const onSubmit = event => {
         event.preventDefault();
         setErrors([]);
@@ -32,41 +28,48 @@ export default function LoginFormPage() {
     const demo = () => {
         dispatch(demoUserLogin())
     }
-
+    
     return(
-        <div>
-            <img src={backgroundImage} alt='background'></img>
-            <div className='textDiv'>
-                <h1>This is what happens</h1> 
-                <h1>when you don't login...</h1>
-            </div>
-            <button onClick={demo}>Demo User</button>
-            <div className='loginFormDiv'>
+        <div className='frame'>
+                <h1 className='login-message'>You Must Be Logged In To View Content</h1>
+            <div className='login-form-div'>
                 <form onSubmit={onSubmit}>
                     <ul>
                         {errors.map((error, i) => <li key={i}>{error}</li>)}
                     </ul>
-                    <label>Enter Credentials
+                    <div className='credentials-div'>
+                        <label>
+                            Enter Credentials
+                        </label>
                         <input
                             type='text'
                             placeholder='Username or Email'
                             value={credential}
                             onChange={event => setCredential(event.target.value)}
                             required
-                            />
-                    </label>
-                    <label>Enter Password
+                        />
+                    </div>
+                    <div className='password-div'>
+                        <label>
+                            Enter Password
+                        </label>
                         <input
+                            className='password-input'
                             type='password'
                             placeholder='Password'
                             value={password}
                             onChange={event => setPassword(event.target.value)}
                             required
-                            />
-                    </label>
-                    <button type='submit'>Login</button>
-                    {/* ¿signup button?) */}
+                        />
+                    </div>
+                    <div className='login-btns'>
+                        <button type='submit'>Login</button>
+                        <button><Link className='signup' to='/signup'>Signup</Link></button>
+                    </div>
                 </form>
+            </div>
+            <div class='demo-btn'>
+                <button onClick={demo}>Demo User</button>
             </div>
         </div>
     );
