f1a79af4bdd737f0cb3c44c8a419794b834aed76
msineath <mikesineath@gmail.com>
2021-10-11 00:50:02 -0400

    updated song comments

--- backend/db/seeders/20211006232413-songComments.js
+++ backend/db/seeders/20211006232413-songComments.js
@@ -7,7 +7,18 @@ module.exports = {
       {songId: 2, comment: 'Best song on the album', userId: 2, createdAt: new Date(), updatedAt: new Date()},
       {songId: 3, comment: 'so-so', userId: 3, createdAt: new Date(), updatedAt: new Date()},
       {songId: 4, comment: 'how is this making money?', userId: 4, createdAt: new Date(), updatedAt: new Date()},
-      {songId: 5, comment: "made me dig out my poi", userId: 5, createdAt: new Date(), updatedAt: new Date()}
+      {songId: 5, comment: "made me dig out my poi", userId: 5, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 6, comment: 'Decent', userId: 1, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 7, comment: 'Wooo!', userId: 2, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 8, comment: 'alright', userId: 3, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 9, comment: 'not too bad', userId: 4, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 10, comment: "haven't heard this in ages...", userId: 5, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 11, comment: 'interesting', userId: 1, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 12, comment: 'love it', userId: 2, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 13, comment: 'catchy', userId: 3, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 14, comment: 'never heard something like this before...', userId: 4, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 15, comment: "play on repeat", userId: 5, createdAt: new Date(), updatedAt: new Date()},
+      {songId: 16, comment: "could take it or leave it", userId: 5, createdAt: new Date(), updatedAt: new Date()},
     ], {});
   },
 
