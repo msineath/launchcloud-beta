c51bde823c691c135512fef676a07345508fe9d5
msineath <mikesineath@gmail.com>
2021-10-10 23:53:04 -0400

    updated albums to reflect demo songs

--- backend/db/seeders/20210927030626-albums.js
+++ backend/db/seeders/20210927030626-albums.js
@@ -3,11 +3,18 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Albums', [
-      {name: "Where the hell am I?!?", createdAt: new Date(), updatedAt: new Date()},
-      {name: "Summoning the Toaster Demons", createdAt: new Date(), updatedAt: new Date()},
-      {name: "{INSERTALBUMNAMEHERE}", createdAt: new Date(), updatedAt: new Date()},
-      {name: "I Lost The Game", createdAt: new Date(), updatedAt: new Date()},
-      {name: "Forever Aloof", createdAt: new Date(), updatedAt: new Date()}
+      {name: "John Doe - Untitled", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Skrillex - Untitled", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Recess", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Rusko - Untitled", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Babylon: Volume 1", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Everyday", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Somewhere Around Barstow / Bopper Shuffle", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Too Much Sexy", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Storm", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Skazi - Untitled", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Spin", createdAt: new Date(), updatedAt: new Date()},
+      {name: "Total Anarchy", createdAt: new Date(), updatedAt: new Date()}
     ], {});
   },
 
